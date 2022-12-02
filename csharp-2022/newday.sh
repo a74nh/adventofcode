847750f705a8e687f9df5302a2256ecbd31ae79d
Alan Hayward <alan.hayward@arm.com>
2022-12-02 09:19:41 +0000

    2022.02

--- csharp-2022/newday.sh
+++ csharp-2022/newday.sh
@@ -8,7 +8,7 @@ mkdir -p input
 
 DAY=$1
 EXDAY=$(printf "%02d" $1)
-YEAR=2021
+YEAR=2022
 
 curl -b session=$(cat ${HOME}/.aocrc) https://adventofcode.com/${YEAR}/day/${DAY}/input > input/day$EXDAY.txt
 
