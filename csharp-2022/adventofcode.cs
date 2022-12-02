77cf37027c20748d7e6c81ee17c375a73b15668b
Alan Hayward <alan.hayward@arm.com>
2022-12-02 13:50:44 +0000

    Add input download

--- csharp-2022/adventofcode.cs
+++ csharp-2022/adventofcode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.Text;
+using System.Net;
 using CommandLine;
 using AdventOfCode;
 
@@ -13,29 +14,42 @@ class Runner {
 
         [Option('t', "test", Required = false, HelpText = "Use test input.")]
         public bool Test { get; set; }
+
+		[Option('d', "download", Required = false, HelpText = "Download input.")]
+        public bool Download { get; set; }
     }
 
     static void Main(string[] args)
     {
         Parser.Default.ParseArguments<CommandLineOptions>(args)
-            .WithParsed<CommandLineOptions>(opts =>
+            .WithParsed( opts =>
             {
-                Solve(opts.Day, opts.Test);
+				if (opts.Download)
+				{
+                    var task = Download(opts.Day);
+					task.Wait();
+				}
+				else
+				{
+					Solve(opts.Day, opts.Test);
+				}
             });
     }
 
+    static string InputFileName(int day, bool test)
+        => string.Format("input/day{0:00}{1}.txt", day, test ? "_test" : "");
+
+
     private static int Solve(int day, bool test)
     {
-        string inputfilename = "";
         string[] input;
         try
         {
-            inputfilename = string.Format("input/day{0:00}{1}.txt", day, test ? "_test" : "");
-            input = System.IO.File.ReadAllLines(inputfilename);
+            input = System.IO.File.ReadAllLines(InputFileName(day, test));
         }
         catch (Exception)
         {
-            System.Console.WriteLine("No input file {0}", inputfilename);
+            System.Console.WriteLine("No input file {0}", InputFileName(day, test));
             return -1;
         }
 
@@ -59,4 +73,56 @@ class Runner {
         System.Console.WriteLine(solve.PartTwo(input));
         return 0;
     }
+
+	private static async Task Download(int day)
+	{
+		var clientHandler = new HttpClientHandler
+		{
+			AllowAutoRedirect = true,
+			UseCookies = true,
+			CookieContainer = new CookieContainer()
+		};
+
+        string[] sessionid;
+        string sessionFileName = string.Format("{0}/.aocrc", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+        try
+        {
+            sessionid = System.IO.File.ReadAllLines(sessionFileName);
+        }
+        catch (Exception)
+        {
+            System.Console.WriteLine("No session id in {0}", sessionFileName);
+            return;
+        }
+
+        string inputFileName = InputFileName(day, false);
+
+        using (var httpClient = new HttpClient(clientHandler))
+		{
+            CookieContainer gaCookies = new CookieContainer();
+            Uri target = new Uri(string.Format("https://adventofcode.com/2022/day/{0}/input",day));
+
+            clientHandler.CookieContainer.Add(new Cookie("session", sessionid[0]) { Domain = target.Host });
+
+            // get the file (cookies are sent automatically).
+            var client = httpClient.GetAsync(target.AbsoluteUri);
+
+            if (!client.Result.IsSuccessStatusCode)
+			{
+                System.Console.WriteLine("Could not download {0}", target.AbsoluteUri);
+                return;
+            }
+
+            HttpContent content = client.Result.Content;
+            var contentStream = await content.ReadAsStreamAsync();
+            using (var fileStream = File.Create(inputFileName))
+            {
+                contentStream.CopyTo(fileStream);
+            }
+		}
+
+        System.IO.File.ReadAllLines(inputFileName).ToList().ForEach(i => Console.WriteLine(i.ToString()));
+        Console.WriteLine("Input saved to {0}", inputFileName);
+	}
+
 }
