f99fe805efc3cc23556b0a8cdec6f23a0cd21a97
Alan Hayward <alan.hayward@arm.com>
2022-12-04 22:35:06 +0000

    Add run all option

---
+++ csharp-2022/Lib/template.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.csharp2022.Day00;
+
+using ParsedData = List<string>;
+
+class Solution : Solver<ParsedData> {
+
+    public override ParsedData Parse(string[] input)
+        => input.ToList();
+
+    public override int DoPartOne(ParsedData input)
+        => -1;
+
+    public override int DoPartTwo(ParsedData input)
+        => -1;
+}
--- csharp-2022/adventofcode.cs
+++ csharp-2022/adventofcode.cs
@@ -9,7 +9,7 @@ class Runner {
 
     public class CommandLineOptions
     {
-        [Value(index: 0, Required = true, HelpText = "Day to solve for.")]
+        [Value(index: 0, Required = true, HelpText = "Day to solve for (0 for all)")]
         public int Day { get; set; }
 
         [Option('t', "test", Required = false, HelpText = "Use test input.")]
@@ -29,6 +29,17 @@ class Runner {
                     var task = Download(opts.Day);
 					task.Wait();
 				}
+                else if (opts.Day == 0)
+                {
+                    int day = 1;
+                    int success;
+                    do
+                    {
+                        System.Console.WriteLine("Day {0}:", day);
+                        success = Solve(day++, opts.Test);
+                    }
+                    while (success == 0);
+                }
 				else
 				{
 					Solve(opts.Day, opts.Test);
@@ -38,7 +49,6 @@ class Runner {
 
     static string InputFileName(int day, bool test)
         => string.Format("input/day{0:00}{1}.txt", day, test ? "_test" : "");
-
 
     private static int Solve(int day, bool test)
     {
@@ -76,13 +86,7 @@ class Runner {
 
 	private static async Task Download(int day)
 	{
-		var clientHandler = new HttpClientHandler
-		{
-			AllowAutoRedirect = true,
-			UseCookies = true,
-			CookieContainer = new CookieContainer()
-		};
-
+        // Get session id.
         string[] sessionid;
         string sessionFileName = string.Format("{0}/.aocrc", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
         try
@@ -95,34 +99,64 @@ class Runner {
             return;
         }
 
+        // Download input file.
         string inputFileName = InputFileName(day, false);
-
-        using (var httpClient = new HttpClient(clientHandler))
-		{
-            CookieContainer gaCookies = new CookieContainer();
-            Uri target = new Uri(string.Format("https://adventofcode.com/2022/day/{0}/input",day));
-
-            clientHandler.CookieContainer.Add(new Cookie("session", sessionid[0]) { Domain = target.Host });
-
-            // get the file (cookies are sent automatically).
-            var client = httpClient.GetAsync(target.AbsoluteUri);
-
-            if (!client.Result.IsSuccessStatusCode)
-			{
-                System.Console.WriteLine("Could not download {0}", target.AbsoluteUri);
-                return;
-            }
-
-            HttpContent content = client.Result.Content;
-            var contentStream = await content.ReadAsStreamAsync();
-            using (var fileStream = File.Create(inputFileName))
+        if (!File.Exists(inputFileName))
+        {
+            var clientHandler = new HttpClientHandler
             {
-                contentStream.CopyTo(fileStream);
-            }
-		}
+                AllowAutoRedirect = true,
+                UseCookies = true,
+                CookieContainer = new CookieContainer()
+            };
+            using (var httpClient = new HttpClient(clientHandler))
+            {
 
-        System.IO.File.ReadAllLines(inputFileName).ToList().ForEach(i => Console.WriteLine(i.ToString()));
-        Console.WriteLine("Input saved to {0}", inputFileName);
+                CookieContainer gaCookies = new CookieContainer();
+                Uri target = new Uri(string.Format("https://adventofcode.com/2022/day/{0}/input",day));
+
+                clientHandler.CookieContainer.Add(new Cookie("session", sessionid[0]) { Domain = target.Host });
+
+                // get the file (cookies are sent automatically).
+                var client = httpClient.GetAsync(target.AbsoluteUri);
+
+                if (!client.Result.IsSuccessStatusCode)
+                {
+                    System.Console.WriteLine("Could not download {0}", target.AbsoluteUri);
+                }
+                else
+                {
+                    HttpContent content = client.Result.Content;
+                    var contentStream = await content.ReadAsStreamAsync();
+                    using (var fileStream = File.Create(inputFileName))
+                    {
+                        contentStream.CopyTo(fileStream);
+                    }
+
+                    System.IO.File.ReadAllLines(inputFileName).ToList()
+                        .ForEach(i => Console.WriteLine(i.ToString()));
+                    Console.WriteLine("Input saved to {0}", inputFileName);
+
+                }
+            }
+        }
+
+        // Create empty test input file.
+        string testInputFile = InputFileName(day, true);
+        if (!File.Exists(testInputFile))
+        {
+            File.Create(testInputFile).Dispose();
+            Console.WriteLine("Created test input file {0}", testInputFile);
+        }
+
+        // Create source code using template.
+        string sourceFile = string.Format("src/Day{0:00}.cs", day);
+        string templateFile = "Lib/template.cs";
+        if (!File.Exists(sourceFile))
+        {
+            File.Copy(templateFile, sourceFile);
+            Console.WriteLine("Created source file {0}", sourceFile);
+        }
 	}
 
 }
