e0d038d926c7daeb8684ce991dcde1994754966f
Alan Hayward <alan.hayward@arm.com>
2022-12-04 13:15:32 +0000

    2022.04

---
+++ csharp-2022/src/Day04.cs
@@ -0,0 +1,31 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.csharp2022.Day04;
+
+using ParsedData = List<List<int>>;
+
+class Solution : Solver<ParsedData> {
+
+    public override ParsedData Parse(string[] input)
+    {
+        Regex regex = new Regex(@"\D+");
+        return input.Select(line => regex.Split(line).Select(i => int.Parse(i)).ToList()).ToList();
+    }
+
+    private bool FullyContained(List<int> ranges)
+        => (ranges[2] >= ranges[0] && ranges[3] <= ranges[1])
+        || (ranges[0] >= ranges[2] && ranges[1] <= ranges[3]);
+
+    private bool Overlaps(List<int> ranges)
+        => (ranges[2] >= ranges[0] && ranges[2] <= ranges[1])
+        || (ranges[3] >= ranges[0] && ranges[3] <= ranges[1])
+        || (ranges[0] >= ranges[2] && ranges[0] <= ranges[3])
+        || (ranges[1] >= ranges[2] && ranges[0] <= ranges[3]);
+
+    public override int DoPartOne(ParsedData input)
+        => input.Select(r => FullyContained(r)).Where(c => c).Count();
+
+    public override int DoPartTwo(ParsedData input)
+        => input.Select(r => Overlaps(r)).Where(c => c).Count();
+}
+
