3ee5f11f641efc99ec25e4946d20d3b1a2557820
Alan Hayward <alan.hayward@arm.com>
2022-12-05 16:11:45 +0000

    2022.05

---
+++ csharp-2022/src/Day05.cs
@@ -0,0 +1,129 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.csharp2022.Day05;
+
+using Board = List<List<char>>;
+using Move = Tuple<int, int, int>;
+using Moves = List<Tuple<int, int, int>>;
+using ParsedData = Tuple<List<List<char>>, List<Tuple<int, int, int>>>;
+
+class Solution : Solver<ParsedData> {
+
+    private void AddToCol(ref Board board, int col, char entry)
+    {
+        List<char> column;
+        try
+        {
+            column = board[col];
+        }
+        catch (Exception)
+        {
+            board.Add(new List<char>());
+            column = board[col];
+        }
+        if (entry == ' ')
+        {
+            return;
+        }
+        column.Add(entry);
+    }
+
+    public override ParsedData Parse(string[] input)
+    {
+        var board = new Board();
+        var moves = new Moves();
+        bool moveSection = false;
+        Regex regex = new Regex(@"\D+");
+        
+        foreach(string line in input)
+        {
+            if (string.IsNullOrEmpty(line) || line[1]=='1')
+            {
+                moveSection = true;
+            }
+            else if (!moveSection)
+            {
+                // Parse board row.
+                int col = 0;
+                int pos = 1;
+                while(true)
+                {
+                    try
+                    {
+                        AddToCol(ref board, col, line[pos]);
+                        col++;
+                        pos+=4;
+                    }
+                    catch (System.IndexOutOfRangeException)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                List<int> split = regex.Split(line)
+                                    .Where(i => !string.IsNullOrEmpty(i))
+                                    .Select(i => int.Parse(i)).ToList();
+                moves.Add(new Move(split[0], split[1], split[2]));
+            }
+        }
+
+        board.ForEach(col => col.Reverse());
+        return new ParsedData(board, moves);
+    }
+
+    public void DoMove9000(ref Board board, int num, int from, int to)
+    {
+        while (num > 0)
+        {
+            // Remove.
+            int last = board[from-1].Count - 1;
+            char piece = board[from-1][last];
+            board[from-1].RemoveAt(last);
+            // Add.
+            board[to-1].Add(piece);
+            num--;
+        }
+    }
+
+    public void DoMove9001(ref Board board, int num, int from, int to)
+    {
+        while (num > 0)
+        {
+            // Remove.
+            int last = board[from-1].Count - num;
+            char piece = board[from-1][last];
+            board[from-1].RemoveAt(last);
+            // Add.
+            board[to-1].Add(piece);
+            num--;
+        }
+    }
+
+    public override int DoPartOne(ParsedData input)
+    {
+        var board = input.Item1;
+        var moves = input.Item2;
+        foreach(Move move in moves)
+        {
+            DoMove9000(ref board, move.Item1, move.Item2, move.Item3);
+        }
+        string ret = new string(board.Select(col => col[col.Count-1]).ToArray());
+        System.Console.WriteLine(ret);
+        return -1;
+    }
+
+    public override int DoPartTwo(ParsedData input)
+    {
+        var board = input.Item1;
+        var moves = input.Item2;
+        foreach(Move move in moves)
+        {
+            DoMove9001(ref board, move.Item1, move.Item2, move.Item3);
+        }
+        string ret = new string(board.Select(col => col[col.Count-1]).ToArray());
+        System.Console.WriteLine(ret);
+        return -1;
+    }
+}
